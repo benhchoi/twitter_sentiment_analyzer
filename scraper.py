724b315a78b94469ee04874d58d90fec9c651fae
Ricky Noll <rickynoll@wustl.edu>
2019-03-20 12:03:11 -0500

    initial scraper

---
+++ scraper.py
@@ -0,0 +1,23 @@
+import tweepy
+import csv
+import pandas as pd
+import sys
+####input your credentials here
+consumer_key = 'YcLDM9cSFJFXWRhRSnL1ZZHEm'
+consumer_secret = 'kN2QIZJ526TD0s0yzmmPjA0PsNRi05h1cruzcX7vbY2SWYpXTB'
+access_token = '854059662857187334-LxA0RdttIzwJZTPiVA3ajIJZYZI9GE1'
+access_token_secret = '5K2QID2WDksSBFcU5RCWXBPfHjpJsaY7dj4QDcpjjdVpl'
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth,wait_on_rate_limit=True)
+#####United Airlines
+# Open/Create a file to append data
+csvFile = open('data.csv', 'a')
+#Use csv Writer
+csvWriter = csv.writer(csvFile)
+
+for tweet in tweepy.Cursor(api.search,q="#covingtoncatholic",count=10000,
+                           lang="en",
+                           since="2019-01-22").items():
+    csvWriter.writerow([tweet.created_at, tweet.text.encode('utf-8')])
